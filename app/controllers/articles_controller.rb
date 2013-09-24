a56a18d46d998670720ce7e17a0defec724a39e3
sid <koolspy.siddhu@gmail.com>
2013-09-24 17:04:53 +0530

    form-based workflow feature completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
+ 	before_filter :authenticate_member!
   def new
+  	@article = Article.new
   end
 
   def index
@@ -8,5 +10,27 @@ class ArticlesController < ApplicationController
   def show
   	@article = Article.find(params[:id])
   end
+  def create 
+  	@article = Article.new
+  	@article.title = params[:article][:title]
+  	@article.body = params[:article][:body]
+  	@article.save
+  	redirect_to article_path(@article)
+  end 
+  def destroy
+  @articledestroy = Article.find(params[:id])
+  @articledestroy.destroy
+  flash.notice = "Data deleted successfully "
+  redirect_to articles_path
+  end
+  def edit
+  	@article = Article.find(params[:id])
+  end
+  def update
+  	@article = Article.find(params[:id])
+  	  	@article.update_attributes(params[:article])
+  	  	redirect_to articles_path
+  end
+
 
 end
